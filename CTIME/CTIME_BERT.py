f80c80bd2f817f45edfa266f819a100988526de7
joshsanyal <josh.sanyal@yahoo.com>
2022-08-28 20:30:48 -0700

    Delete CTIME directory

--- CTIME/CTIME_BERT.py
+++
@@ -1,9 +0,0 @@
-import torch
-import transformers
-from transformers import BertTokenizer, BertModel, BertForMaskedLM, BertForSequenceClassification
-
-
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-
-
-model = BertForSequenceClassification.from_pretrained()
